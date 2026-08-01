8e4d696bf0d1044ea9a2a7777795c34206078d21
Mohd Shaquib Ansari <118913864+mohdshaquibansari@users.noreply.github.com>
2026-08-01 12:31:57 +0530

    Time: 0 ms (100%), Space: 42.8 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] memo = new Integer[n][n];
+        return maxDiff(nums, 0, n - 1, memo) >= 0;
+    }
+
+    private int maxDiff(int[] nums, int left, int right, Integer[][] memo) {
+       
+        if (left == right) {
+            return nums[left];
+        }
+        if (memo[left][right] != null) {
+            return memo[left][right];
+        }
+
+        int pickLeft = nums[left] - maxDiff(nums, left + 1, right, memo);
+        int pickRight = nums[right] - maxDiff(nums, left, right - 1, memo);
+
+        return memo[left][right] = Math.max(pickLeft, pickRight);
+    }
+}
